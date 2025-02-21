ddd84f46fb32c2d724c9224d7bb420df2567fc9e
opeyemi2007 <opeyemiadio2007@gmail.com>
2025-02-21 18:57:53 +0100

    hgvj

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
     const navigate = useNavigate()
   return (
     <div className='welcomePageWrapper'>
-        <img src="public/TasksManagement.jpeg" alt="Task Management" />
+        <img src="/TasksManagement.jpeg" alt="Task Management" />
         <h1>Welcome to Lyra Todo App</h1>
         <span>Stay organized, focused, and in  control with Lyra — your<br /> ultimate to-do list companion.
         Whether it's daily tasks or <br /> long-term goals, Lyra helps you plan, prioritize, and achieve with ease.</span>
