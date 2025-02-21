8548946f9ca614b95515d2a6e355e42f5dedb978
opeyemi2007 <opeyemiadio2007@gmail.com>
2025-02-21 18:20:11 +0100

    image

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -6,13 +6,15 @@ import { useNavigate } from 'react-router-dom';
 const Welcome = () => {
     const navigate = useNavigate()
   return (
+    <>
     <div className='welcomePageWrapper'>
-        <img src="src/assets/Premium Vector _ Tasks management and work organization flat illustration.jpeg" alt="" />
+        <img src={require('src/assets/Premium Vector _ Tasks management and work organization flat illustration.jpeg')} />
         <h1>Welcome to Lyra Todo App</h1>
         <span>Stay organized, focused, and in  control with Lyra — your<br /> ultimate to-do list companion.
         Whether it's daily tasks or <br /> long-term goals, Lyra helps you plan, prioritize, and achieve with ease.</span>
         <button className="getstartedBtn" onClick={()=> navigate('/private')}>Get Started <MdStart /></button>
     </div>
+    </>
   )
 }
 
